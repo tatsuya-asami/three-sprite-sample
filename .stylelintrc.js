9ffa4882fac637eef3dffcd2174d0653dbc3f4ba
Tatsuya Asami <sharekobekko@gmail.com>
2020-07-12 00:56:46 +0900

    Initial commit

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-recess-order',
+    'stylelint-prettier/recommended',
+  ],
+  // フォーマットの設定はここでは行わない。全て.prettierrc.jsで行う。
+};
